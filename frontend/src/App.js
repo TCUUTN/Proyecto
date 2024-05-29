f2e82015bbfa9eb2c3d8c518d0efd141293dc790
Israel Calvo Morera <icalvom@est.utn.ac.cr>
2024-05-28 23:06:41 -0600

    Cambios para pull

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -10,21 +10,13 @@ import FooterPage from './pages/FooterPage';
 
 
   function App() {
-    const [data, setData] = useState([]);
-  
-    useEffect(() => {
-      fetch('/usuarios/')
-        .then(response => response.json())
-        .then(data => setData(data))
-        .catch(error => console.error(error));
-    }, []);
   
     return (
       <Router>
         <div className="App">
           <Navbar />
           <Routes>
-            <Route path="/" element={<Home data={data} />} />
+            <Route path="/" element={<Home/>} />
             <Route path="/contact" element={<Contact />} />
             <Route path="/footer" element={<FooterPage />} />
           </Routes>
