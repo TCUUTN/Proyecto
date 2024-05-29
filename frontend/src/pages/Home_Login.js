0e359cfae273c047e08a71d403a09ba12b740f9a
Israel Calvo Morera <icalvom@est.utn.ac.cr>
2024-05-28 23:16:23 -0600

    Login Funcionando

--- frontend/src/pages/Home_Login.js
+++ frontend/src/pages/Home_Login.js
@@ -1,7 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Home.css';
 
 function Home() {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    try {
+      const response = await fetch('/usuarios/credenciales', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ CorreoElectronico: username, Contrasenna: password })
+      });
+
+      if (response.ok) {
+        // Si las credenciales son correctas, redirige a la página Contact.js
+        window.location.href = '/contact';
+      } else {
+        // Si las credenciales son incorrectas, muestra una notificación
+        setError('Usuario o contraseña incorrectos');
+      }
+    } catch (error) {
+      console.error('Error al enviar la solicitud:', error);
+    }
+  };
+
   return (
     <div className="home-container">
       <div className="home-content">
@@ -15,15 +43,28 @@ function Home() {
         <h2 className="login-title">Iniciar Sesión</h2>
         <form>
           <label htmlFor="username">Nombre de usuario:</label>
-          <input type="text" id="username" name="username" />
+          <input 
+            type="text" 
+            id="username" 
+            name="username" 
+            value={username}
+            onChange={(e) => setUsername(e.target.value)} 
+          />
 
           <label htmlFor="password">Contraseña:</label>
-          <input type="password" id="password" name="password" />
+          <input 
+            type="password" 
+            id="password" 
+            name="password" 
+            value={password}
+            onChange={(e) => setPassword(e.target.value)} 
+          />
 
-          <button type="submit">Acceder</button>
+          <button type="submit" onClick={handleSubmit}>Acceder</button>
 
           <p>¿Olvidó su contraseña?</p>
         </form>
+        {error && <p className="error-message">{error}</p>}
       </div>
     </div>
   );
