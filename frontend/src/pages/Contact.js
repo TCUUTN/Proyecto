e6566ea7b7a7d4087ff7329b1704eafad3c47862
VALERIA BARQUERO CHANG <vbarquero@est.utn.ac.cr>
2024-05-28 21:10:35 -0600

    Home y Navbar

---
+++ frontend/src/pages/Contact.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Contact() {
+  return (
+    <div>
+      <h1>Contacto</h1>
+      <p>Información de contacto aquí.</p>
+    </div>
+  );
+}
+
+export default Contact;
