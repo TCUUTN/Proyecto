cc531f6fc596acaa64eb59dff0c8bc0981a326d9
Israel Calvo Morera <icalvom@est.utn.ac.cr>
2024-05-28 23:01:38 -0600

    Backend de Login

--- backend/controllers/seguridadController.js
+++ backend/controllers/seguridadController.js
@@ -10,6 +10,32 @@ const getAllUsuarios = async (req, res) => {
   }
 };
 
+const getUsuarioPorCredenciales = async (req, res) => {
+  try {
+    const { CorreoElectronico, Contrasenna } = req.body; // Suponiendo que estás enviando los parámetros en el cuerpo de la solicitud
+
+    // Buscar un usuario que coincida con el correo electrónico y la contraseña proporcionados
+    const usuario = await Usuario.findOne({
+      where: {
+        CorreoElectronico: CorreoElectronico,
+        Contrasenna: Contrasenna
+      }
+    });
+
+    // Si se encuentra un usuario, devolverlo en la respuesta
+    if (usuario) {
+      res.json(usuario);
+    } else {
+      // Si no se encuentra ningún usuario que coincida, devolver un mensaje de error
+      res.status(401).json({ error: 'Usuario o contraseña inválida' });
+    }
+  } catch (err) {
+    // Manejo de errores
+    res.status(500).json({ error: err.message });
+  }
+};
+
   module.exports = {
-    getAllUsuarios
+    getAllUsuarios,
+    getUsuarioPorCredenciales
   };
--- backend/routes/seguridadRoute.js
+++ backend/routes/seguridadRoute.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const seguridadController = require('../controllers/seguridadController');
 
 router.get('/', seguridadController.getAllUsuarios);
+router.post('/credenciales', seguridadController.getUsuarioPorCredenciales);
 
 router.get('/test', (req, res) => {
     res.send('Hello from exampleRoute!');
