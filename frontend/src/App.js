fea7d5d26a04e501e15f957c64e6ec31e699bbbf
Israel Calvo Morera <icalvom@est.utn.ac.cr>
2024-05-30 23:38:10 -0600

    Cambios Inicio Sesion

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,7 +4,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Contact from './pages/Contact';
 import Home from './pages/Home_Login';
-import FooterPage from './pages/FooterPage';
+import Footer from './components/Footer';
 import InicioAdmin  from './pages/Inicio/Home_Admin';
 
 
@@ -19,10 +19,9 @@ import InicioAdmin  from './pages/Inicio/Home_Admin';
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/contact" element={<Contact />} />
-            <Route path="/footer" element={<FooterPage />} />
             <Route path="/Home_Admin" element={<InicioAdmin/>} />
           </Routes>
-
+          <Footer />
       </div>
       </Router>
     );
--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -4,7 +4,7 @@ import './Footer.css';
 function Footer() {
   return (
     <footer className="footer">
-      © Universidad Técnica Nacional
+      &copy; {new Date().getFullYear()} Universidad Técnica Nacional. Todos los derechos reservados.
     </footer>
   );
 }
