be853538a5390d2ead02adc238d810ef7733ec98
ValBarq <vbarquero@est.utn.ac.cr>
2024-06-30 20:01:04 -0600

    Archivos de ver Solicitud

---
+++ frontend/src/pages/SociosComunitarios/VerSolicitudes.js
@@ -0,0 +1,14 @@
+import React, { useState, useEffect } from "react";
+import "react-toastify/dist/ReactToastify.css";
+
+
+function VerSolicitudes (){
+    return (
+        <div>
+
+        </div>
+    )
+
+}
+
+export default VerSolicitudes
